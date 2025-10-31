c5547786536e8868c9ac273d5f58445321717c0b
razzlerazing2 <deepvishwakarma037@gmail.com>
2025-10-31 00:51:38 +0000

    4wjesf

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
 const config = {
-  challenge: true, // set to true to enable password protection
+  challenge: false, // set to true to enable password protection
   users: {
     user_one: "bypass"
   }
 };
 
 export default config;
-
-// What are you doing here if you are here u are 🏳️‍🌈
-// jk u are not THAT but still use npm start and then npm start
-// share this w/ ur friends :D
+// user_one is the username, bypass is the password
--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const CACHE_TTL = 30 * 24 * 60 * 60 * 1000; // Cache for 30 Days
 
 if (config.challenge === true) {
   console.log(
-    chalk.green("🔒 Password protection is enabled! Listing logins below")
+    chalk.green("Password protection is enabled. It's for teachers if they want to hop on.")
   );
   // biome-ignore lint/complexity/noForEach:
   Object.entries(config.users).forEach(([username, password]) => {
